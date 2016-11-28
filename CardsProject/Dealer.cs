c42183c4125abf2a583991d7db08b571e1bab6ce
Greg Doud <gpdoud@gmail.com>
2016-11-28 15:55:06 -0500

    Documented the Dealer class

--- CardsProject/Dealer.cs
+++ CardsProject/Dealer.cs
@@ -5,7 +5,17 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace GregoryDoud {
+	/// <summary>
+	/// A Dealer is a special Player for blackjack because
+	/// of the mandatory rules about drawing cards.
+	/// </summary>
 	public class Dealer : Player {
+		/// <summary>
+		/// Determines whether the dealer must
+		/// draw a card or not. For the dealer,
+		/// an Ace is always 11.
+		/// </summary>
+		/// <returns></returns>
 		public bool MustDraw() {
 			// a dealer must draw if total < 17
 			// ace is always 11 for the dealer
@@ -17,6 +27,10 @@ namespace GregoryDoud {
 			}
 			return max < 17;
 		}
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="name"></param>
 		public Dealer(string name) : base(name) {
 		}
 	}
